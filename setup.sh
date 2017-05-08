c5a2728a275abbd5bd59f925712b05710602989b
Weilu Jia <weilu.jia@gusto.com>
2017-05-08 11:59:18 -0700

    Patch zshrc in setup script for macOS Sierra
    Fixes battery meter issue

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-set -e
+# Quick installer for dotfiles
 # USAGE: curl -L https://github.com/optix2000/dotfiles/raw/master/setup.sh | bash
+set -e
 GITURL='https://github.com/optix2000/dotfiles.git'
 DOTDIR='dotfiles'
 DOTSUBDIR='dotfiles'
@@ -8,9 +9,17 @@ TMPDIR=`mktemp -d`
 if [ -z "$TMPDIR" ]; then
     exit 1
 fi
+
 cd $TMPDIR
 git clone --depth 1 --recursive $GITURL $DOTDIR
 cd $DOTDIR
+
+# Patch zshrc on macOS 16 (Sierra)
+if [[ "$OSTYPE" == "darwin16"* ]]; then
+  patch $DOTDIR/.zshrc zshrc-macos-sierra-battery.patch
+fi
+
+
 # Glob dotfiles
 shopt -s dotglob nullglob
 rsync -rvvbcl $DOTSUBDIR/* ~/
